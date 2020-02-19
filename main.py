be5c7ce221a65be589c942188a99873aedea9cf2
lipengfei <hudalpf@163.com>
2020-02-20 02:08:32 +0800

    音乐推荐系统

--- main.py
+++ main.py
@@ -5,14 +5,44 @@ from model import predict
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def demo():
+@app.route('/index.html', methods=['GET', 'POST'])
+def recommendation1():
   if request.method == 'GET':
-    return render_template('index.html', input_text = '', res_text = '')
+    return render_template('index.html', input_text ='', res_text ='')
   else:
     inputText = request.form.get("input_text")
     result_list = predict(str(inputText))
-    resText = Markup(formatRes(result_list))
-    return render_template('index.html', input_text = inputText, result_text = resText)
+    result_text = Markup(formatRes(result_list))
+    return render_template('index.html', input_text = inputText, res_text = result_text)
+
+@app.route('/rec2.html', methods=['GET', 'POST'])
+def recommendation2():
+  if request.method == 'GET':
+    return render_template('rec2.html', input_text = '', res_text = '')
+  else:
+    inputText = request.form.get("input_text")
+    result_text = predict(str(inputText))
+    return render_template('rec2.html', input_text = inputText, res_text = result_text)
+
+@app.route('/rec3.html', methods=['GET', 'POST'])
+def recommendation3():
+  if request.method == 'GET':
+    return render_template('rec3.html', input_text = '', res_text = '')
+  else:
+    inputText = request.form.get("input_text")
+    result_list = predict(str(inputText))
+    result_text = Markup(formatRes(result_list))
+    return render_template('rec3.html', input_text = inputText, res_text = result_text)
+
+@app.route('/rec4.html', methods=['GET', 'POST'])
+def recommendation4():
+  if request.method == 'GET':
+    return render_template('rec4.html', input_text = '', res_text = '')
+  else:
+    inputText = request.form.get("input_text")
+    result_list = predict(str(inputText))
+    result_text = Markup(formatRes(result_list))
+    return render_template('rec4.html', input_text = inputText, res_text = result_text)
 
 
 def formatRes(textList):
